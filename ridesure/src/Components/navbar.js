2b40cc0ea6409509172314be5f9bfa8821990169
kiago <roy.kiago@student.moringaschool.com>
2025-04-20 16:22:05 +0300

    Navbar

---
+++ ridesure/src/Components/navbar.js
@@ -0,0 +1,19 @@
+import react from "react";
+import "./App.css";
+const Navbar =()=>{
+    <nav className="navbar">
+            <div className="navbar-logo">CarHire</div>
+                <ul className="navbar-links">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Browse Cars</a></li>
+                <li><a href="#">Bookings</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        <div className="navbar-auth">
+            <button className="btn login">Login</button>
+            <button className="btn signup">Sign Up</button>
+        </div>
+    </nav>
+}
+
+export default Navbar
